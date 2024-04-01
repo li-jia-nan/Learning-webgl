d320bad328fbd9810f05b3d1ea26a95d4166a3ba
栗嘉男 <574980606@qq.com>
2024-04-01 19:05:54 +0800

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,39 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React from "react";
+import initShader from "./initShader";
+import styles from "./app.module.scss";
+
+const VERTEX_SHADER = /*glsl*/ `
+  attribute vec4 a_Position;
+  void main() {
+    gl_Position = a_Position;
+    gl_PointSize = 10.0;
+  }
+`;
+
+const FRAGMENT_SHADER = /*glsl*/ `
+  void main() {
+    gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
+  }
+`;
 
 const App: React.FC = () => {
-  return <div>学习webgl</div>;
+  const canvasRef = React.useRef<HTMLCanvasElement>(null);
+  const gl = canvasRef.current?.getContext("webgl");
+  React.useEffect(() => {
+    if (gl) {
+      const program = initShader(gl, VERTEX_SHADER, FRAGMENT_SHADER);
+      const aPosition = gl.getAttribLocation(program, "aPosition");
+      const points = new Float32Array([-0.5, -0.5, 0.5, -0.5, 0.0, 0.5]);
+      const buffer = gl.createBuffer();
+      gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
+      gl.bufferData(gl.ARRAY_BUFFER, points, gl.STATIC_DRAW);
+      gl.vertexAttribPointer(aPosition, 2, gl.FLOAT, false, 0, 0);
+      gl.enableVertexAttribArray(aPosition);
+      gl.drawArrays(gl.POINTS, 0, 3);
+    }
+  }, []);
+  return <canvas ref={canvasRef} className={styles.canvas} />;
 };
 
 export default App;
---
+++ src/initShader.ts
@@ -0,0 +1,24 @@
+const initShader = (
+  gl: WebGLRenderingContext,
+  VERTEX_SHADER: string,
+  FRAGMENT_SHADER: string,
+): WebGLProgram => {
+  const vertexShader = gl.createShader(gl.VERTEX_SHADER)!;
+  const fragmentShader = gl.createShader(gl.FRAGMENT_SHADER)!;
+
+  gl.shaderSource(vertexShader, VERTEX_SHADER);
+  gl.shaderSource(fragmentShader, FRAGMENT_SHADER);
+
+  gl.compileShader(vertexShader);
+  gl.compileShader(fragmentShader);
+
+  const program = gl.createProgram()!;
+
+  gl.attachShader(program, vertexShader);
+  gl.attachShader(program, fragmentShader);
+  gl.linkProgram(program);
+  gl.useProgram(program);
+  return program;
+};
+
+export default initShader;
